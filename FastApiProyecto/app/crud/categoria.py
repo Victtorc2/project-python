ff29a811fc533f97322e9e7a3a2122386e67fdd8
Victtorc2 <viccx20004@hotmail.com>
2025-07-09 16:22:18 -0500

    Corrigiendo Procfile para deploy en Railway

--- FastApiProyecto/app/crud/categoria.py
+++ FastApiProyecto/app/crud/categoria.py
@@ -1,32 +1,32 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session # Importamos la clase Session de SQLAlchemy para manejar la sesión de la base de datos
 from app.models.categoria import Categoria  # archivo renombrado
 from app.schemas.categoria import CategoriaCreate, CategoriaUpdate
 
 
-def get_all(db: Session):
-    return db.query(Categoria).all()
+def get_all(db: Session):  #Metodo para obtener todas las categorías de la base de datos
+    return db.query(Categoria).all() #devolvemos todas las categorías
 
-def get_by_id(db: Session, categoria_id: int):
-    return db.query(Categoria).filter(Categoria.id == categoria_id).first()
+def get_by_id(db: Session, categoria_id: int): #Metodo para obtener una categoría por su ID
+    return db.query(Categoria).filter(Categoria.id == categoria_id).first() #devolvemos la categoría con el ID especificado filtando por el ID
 
-def create(db: Session, categoria: CategoriaCreate):
-    nueva = Categoria(**categoria.dict())
+def create(db: Session, categoria: CategoriaCreate): #Metodo para crear una nueva categoría
+    nueva = Categoria(**categoria.dict()) #creamos una nueva categoría usando el modelo Categoria y los datos del esquema CategoriaCreate
     db.add(nueva)
-    db.commit()
-    db.refresh(nueva)
-    return nueva
+    db.commit() #ejecutamos la transacción para guardar los cambios en la base de datos
+    db.refresh(nueva) #actualizamos la instancia de la categoría con los datos de la base de datos
+    return nueva #devolvemos la nueva categoría creada
 
 def update(db: Session, categoria_id: int, datos: CategoriaUpdate):
     categoria = get_by_id(db, categoria_id)
     if categoria:
-        for key, value in datos.dict().items():
-            setattr(categoria, key, value)
+        for key, value in datos.dict().items(): # Iteramos sobre los datos del esquema CategoriaUpdate
+            setattr(categoria, key, value) # Asignamos el valor al atributo correspondiente de la categoría
         db.commit()
         db.refresh(categoria)
     return categoria
 
 def delete(db: Session, categoria_id: int):
-    categoria = get_by_id(db, categoria_id)
+    categoria = get_by_id(db, categoria_id) # Obtenemos la categoría por su ID
     if categoria:
         db.delete(categoria)
         db.commit()
