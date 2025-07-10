682a3321e4f4ac6772713d6194fef1352a09d770
Victtorc2 <viccx20004@hotmail.com>
2025-07-09 23:24:57 -0500

    ventas

--- FastApiProyecto/app/models/Venta.py
+++ FastApiProyecto/app/models/Venta.py
@@ -10,6 +10,6 @@ class Venta(Base):
     cliente = Column(String(100), nullable=False)
     vendedor = Column(String(100), nullable=False)
     total = Column(Float, nullable=False)
-    fecha = Column(DateTime, default=datetime.utcnow)
+    fecha = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     detalles = relationship("VentaDetalle", back_populates="venta", cascade="all, delete-orphan")
